e56887defc09ee057f83013417a208512fbb3c36
Dinh Nam <dvnam1010@gmail.com>
2019-10-10 07:34:43 +0700

    commit faq

--- src/app/modules/product/product/product-form/product-attribute/model/product-value.model.ts
+++ src/app/modules/product/product/product-form/product-attribute/model/product-value.model.ts
@@ -1,4 +1,4 @@
-import { AttributeValueViewModel } from '../../../../product-attribute/product-attribute-value/product-attribute-value.viewmodel';
+import {AttributeValueViewModel} from '../../../../product-attribute/product-attribute-value/product-attribute-value.viewmodel';
 
 export class ProductAttribute {
     id?: string;
@@ -12,14 +12,16 @@ export class ProductAttribute {
     isSelfContent: boolean;
     isShowClient?: boolean;
     languageId?: string;
+    isRequired?: boolean;
 
     constructor(attributeId?: string,
                 value?: string,
                 attributeValues?: AttributeValueViewModel[],
-                isShowClient?: boolean) {
+                isShowClient?: boolean, isRequired?: boolean) {
         this.attributeId = attributeId;
         this.value = value;
         this.attributeValues = attributeValues;
         this.isShowClient = isShowClient;
+        this.isRequired = isRequired;
     }
 }
